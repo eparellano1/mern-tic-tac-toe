eb3e7a5a32d2999c1bce0eafb9908a039f6fd1c0
eparellano1 <eparellano1@up.edu.ph>
2024-03-21 16:38:55 +0800

    update ports

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3001;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // Middleware
